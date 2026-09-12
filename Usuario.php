10d181df34f23fa7d51f619174aca0a04f7ab574
EnzoAlberti <enzogfalbert@gmail.com>
2026-09-11 22:12:17 -0300

    php dnv

--- Usuario.php
+++ Usuario.php
@@ -1,25 +1,43 @@
 <?php
-    class Usuario{
-        private $idade;
-        private $idade;
-        private $email;
-        //public $idade;
-        //public $idade;
-        //public $email;
-        public function getNome(){
-            return $this-> nome;
-        }
-        public function setNome($nome){
-            $this->nome = $nome;
-            
-        }
-        public fuction setIdade($idade){
-            $this->idade = $idade;
-        }
-        public fuction setEmail($email){
-            $this->email = $email;
-        }
 
+class Usuario {
+
+    private $nome;
+    private $idade;
+    private $email;
+    //public $nome;
+    //public $idade;
+    //public $email;
+}
+
+public function __construct($nome, $idade, $email) {
+    $this->nome = $nome;
+    $this->idade = $idade;
+    $this->setEmail($email);
+}
+
+public function getNome() {
+    return $this->nome;
+}
+
+public function setNome($nome) {
+    $this->nome = $nome;
+}
+
+public function setIdade($idade) {
+    $this->idade = $idade;
+}
+
+public function getEmail() {
+    return $this->email;
+}
+
+public function setEmail($email) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $this->email = $email;
+    } else {
+        echo "<p>Email inválido</p>";
     }
+}
 
-    ?>
+?>
--- index.php
+++ index.php
@@ -1,36 +1,48 @@
 <?php
-    require "Usuario.php";
+require "Usuario.php";
 
-    $aluno= new Usuario();
-    $aluno ->nome = "João";
-    $aluno ->idade = 20;
-    $aluno -> email = "maria@ig.com.br";
+$aluno= new Usuario("João",22,);
+$aluno ->nome = "João";
+$aluno ->idade = 20;
+$aluno -> email = "maria@ig.com.br";
 
-    $aluno2= new Usuario();
-    $aluno2 ->nome = "Joaquim";
-    $aluno2 -> idade = 30;
-    $aluno2 -> email = "joaquim@irc.com.br";
+$aluno2= new Usuario("Joaquim",30,);
+$aluno2 ->nome = "Joaquim";
+$aluno2 -> idade = 30;
+$aluno2 -> email = "joaquim@irc.com.br";
 
-    var_dump($aluno);
-    echo "<br>";
-    var_dump($aluno2);*/
+var_dump($aluno);
+echo "<br>";
+var_dump($aluno2);
 
-    $aluno= new Usuario();
-    $aluno -> setNome ("João");
-    $aluno -> setIdade (20);
-    $aluno -> setEmail ("maria@ig.com.br");
+$aluno= new Usuario("João",20,);
+$aluno -> setNome ("João");
+$aluno -> setIdade (20);
+$aluno -> setEmail ("maria@ig.com.br");
 
-    $aluno2= new Usuario();
-    $aluno2 -> setNome ("Joaquim");
-    $aluno2 -> setIdade (30);
-    $aluno2 -> setEmail ("joaquim@irc.com.br");
+$aluno2= new Usuario("João",22,);
+$aluno2 -> setNome ("Joaquim");
+$aluno2 -> setIdade (30);
+$aluno2 -> setEmail ("joaquim@irc.com.br");
 
-    echo "<p>Nome: {$aluno ->getNome()}</p>";
-    echo "<p>Idade: {$aluno ->getIdade()}</p>";
-    echo "<p>Email: {$aluno ->getEmail()}</p>";
-    echo "<br>";
-    echo "<p>Nome: {$aluno2 ->getNome()}</p>";
-    echo "<p>Idade: {$aluno2 ->getIdade()}</p>";
-    echo "<p>Email: {$aluno2 ->getEmail()}</p>";
+echo "<p>Nome: {$aluno ->getNome()}</p>";
+echo "<p>Idade: {$aluno ->getIdade()}</p>";
+echo "<p>Email: {$aluno ->getEmail()}</p>";
+echo "<br>";
+echo "<p>Nome: {$aluno2 ->getNome()}</p>";
+echo "<p>Idade: {$aluno2 ->getIdade()}</p>";
+echo "<p>Email: {$aluno2 ->getEmail()}</p>";
+
+$aluno3= new Usuario("Epnhaminondas",17,);
+$aluno3 -> setNome ("Ephaminondas");
+$aluno3 -> setIdade (17);
+$aluno3 -> setEmail ("ephaminondas#cqc.com.br");
+echo "<br>";
+echo "<p>Idade: {$aluno3->getNome()}</p>";
+echo "<p>Email: {$aluno3-> getEmail()}</p";
+
+$aluno4= new Usuario("Alice",22,);
+echo "<br>";
+var_dump($aluno4);
 
 ?>
