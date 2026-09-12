7668650ac11c8d99f87410c674e17c1ea032a302
EnzoAlberti <enzogfalbert@gmail.com>
2026-09-11 22:07:40 -0300

    php classe

---
+++ Usuario.php
@@ -0,0 +1,25 @@
+<?php
+    class Usuario{
+        private $idade;
+        private $idade;
+        private $email;
+        //public $idade;
+        //public $idade;
+        //public $email;
+        public function getNome(){
+            return $this-> nome;
+        }
+        public function setNome($nome){
+            $this->nome = $nome;
+            
+        }
+        public fuction setIdade($idade){
+            $this->idade = $idade;
+        }
+        public fuction setEmail($email){
+            $this->email = $email;
+        }
+
+    }
+
+    ?>
--- index.php
+++ index.php
@@ -1,35 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Teste Jogo</title>
-</head>
-<body>
-    <h1>Acerte o número!</h1>
-    <form action="index.php" method="post">
-        <label>Digite um número: </label>
-        <input type="number" name="txtnum">
-    </form>
-    <?php
-        $num = get_POST['txtnum'];
-        $na = random_int(1,50);
-        $r = false;
+<?php
+    require "Usuario.php";
 
-        while($r != true){
-            if($num > $na){
-                echo "<p>O número $num, é maior que o número aleatorio.</p>";
-            }elseif($num < $na){
-                echo "<p> o número $num, é menor que o número aleatorio</p>";
-            }else{
-                echo "<p>pabens! Você acertou o número.";
-                $r = true;
-            }
-            echo "<form action='index.php' method='post'>";
-            echo    "<label>Digite um número: </label>";
-            echo    "<input type='number' name='txtnum'>";
-            echo "</form>"; 
-        }
-    ?>
-</body>
-</html>
+    $aluno= new Usuario();
+    $aluno ->nome = "João";
+    $aluno ->idade = 20;
+    $aluno -> email = "maria@ig.com.br";
+
+    $aluno2= new Usuario();
+    $aluno2 ->nome = "Joaquim";
+    $aluno2 -> idade = 30;
+    $aluno2 -> email = "joaquim@irc.com.br";
+
+    var_dump($aluno);
+    echo "<br>";
+    var_dump($aluno2);*/
+
+    $aluno= new Usuario();
+    $aluno -> setNome ("João");
+    $aluno -> setIdade (20);
+    $aluno -> setEmail ("maria@ig.com.br");
+
+    $aluno2= new Usuario();
+    $aluno2 -> setNome ("Joaquim");
+    $aluno2 -> setIdade (30);
+    $aluno2 -> setEmail ("joaquim@irc.com.br");
+
+    echo "<p>Nome: {$aluno ->getNome()}</p>";
+    echo "<p>Idade: {$aluno ->getIdade()}</p>";
+    echo "<p>Email: {$aluno ->getEmail()}</p>";
+    echo "<br>";
+    echo "<p>Nome: {$aluno2 ->getNome()}</p>";
+    echo "<p>Idade: {$aluno2 ->getIdade()}</p>";
+    echo "<p>Email: {$aluno2 ->getEmail()}</p>";
+
+?>
